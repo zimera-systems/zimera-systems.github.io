a0f093c6ec2d498d43a0a6825466df14aba29843
Bambang Purnomosidi D. P <phylossophie@gmail.com>
2021-03-07 12:59:23 +0700

    add router and about page

---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>About Zimera Systems</h1>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <div class="hello">
+      <h1>Welcome to Zimera Systems</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home'
+}
+</script>
